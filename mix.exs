13c79bf1e464558b2c61b89c980a509de60ac59b
Evgeny Golyshev <grga-pitic@yandex.ru>
2014-05-23 17:47:10 +0400

    Теперь нет необходимости выполнять :application.start(:crypto) и :application.start(:emysql) перед началом работы.

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Sqlex.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    []
+    [ applications: [:emysql, :crypto] ]
   end
 
   # Returns the list of dependencies in the format:
