2ca1eac0a370fa07580a3f72daff38def095fb84
Igor Potapov <scsiboy@gmail.com>
2014-09-23 18:27:57 +0200

    Fix atoms

--- lib/sqlex.ex
+++ lib/sqlex.ex
@@ -51,13 +51,15 @@ defmodule SQL do
 	end
 
     defp prep_argument(nil), do: 'NULL'
+    defp prep_argument(true), do: 'true'
+    defp prep_argument(false), do: 'false'
     defp prep_argument(:null), do: 'NULL'
     defp prep_argument(:undefined), do: 'NULL'
     defp prep_argument(arg) when is_list(arg), do: [[?(| String.to_char_list Enum.join quote_if_needed(arg), "," ]|[?)]]
     defp prep_argument(arg) when is_binary(arg), do: [[?'|escape(String.to_char_list(arg))]|[?']]
+    defp prep_argument(arg) when is_atom(arg), do: [[?'|escape(Atom.to_char_list(arg))]|[?']]
     defp prep_argument(arg) when is_integer(arg), do: Integer.to_char_list arg
     defp prep_argument(arg) when is_float(arg), do: Float.to_char_list arg
-    defp prep_argument(arg) when is_atom(arg), do: Atom.to_char_list arg
 
 	defp in_query([], _), do: []
 	defp in_query(sql, args) when is_binary(sql), do: in_query(:erlang.binary_to_list(sql), args)
